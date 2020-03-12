df97b4dc72ddf6fb346680ab04af0962a5e94e64
ClockWorkMini <49729188+mochiponzu@users.noreply.github.com>
2020-03-12 21:50:44 +0100

    Item Icons, Footsteps

---
+++ Assets/AnimatorEvents.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorEvents : MonoBehaviour
+{
+    public AudioClip footStep;
+    AudioSource soundSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FootStepSound() {
+        soundSource.PlayOneShot(footStep);
+    }
+}
